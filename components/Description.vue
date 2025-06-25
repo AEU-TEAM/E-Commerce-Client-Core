7d50e9729020aea6abe6c4a462783cf74329d12d
Oun Bav <oun.bav@igtech.asia>
2025-06-26 01:24:18 +0700

    enhance product shop and detail

---
+++ components/Description.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card flat class="pa-2">
+    <p>
+      Enhanced capabilities thanks to an enlarged display of 6.7 inches and work without recharging throughout the day.
+      Incredible photos in weak, and in bright lighting using the new system with two cameras.
+    </p>
+  </v-card>
+</template>
+
+<script setup>
+// No script needed for static text
+</script>
---
+++ pages/product/[id].vue
@@ -0,0 +1,40 @@
+<template>
+  <Navbar />
+  <v-container class="mt-15" fluid>
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-img :src="product.thumbnail" height="400px" class="rounded-xl" />
+      </v-col>
+      <v-col cols="12" md="6">
+        <ProductInfo :product="product" @add-to-cart="addToCart" />
+      </v-col>
+    </v-row>
+
+    <v-divider class="my-6"></v-divider>
+
+    <ProductReviews :reviews="product.reviews" />
+
+    <ReviewForm :productId="product.id" @submit="submitReview" />
+  </v-container>
+  <Footer />
+</template>
+
+<script setup>
+import { useRoute } from "vue-router";
+import ProductInfo from "@/components/Product/ProductInfo.vue";
+import ProductReviews from "@/components/Product/ProductReviews.vue";
+import ReviewForm from "@/components/Product/ReviewForm.vue";
+
+const productStore = useProductStore();
+const product = computed(() => productStore.selectedProduct);
+// Simulated product fetching
+const route = useRoute();
+
+const addToCart = () => {
+  alert("Added to cart!");
+};
+
+const submitReview = (review) => {
+  product.value.reviews.push(review);
+};
+</script>
